804acd9f817b7a5c98dfaedd2a62db523c9b15cb
Blaire Baker <bakerblaire@gmail.com>
2020-11-19 19:35:08 -0600

    commit tree 411e71488f0ac64da845f6da2e5c90603a21bffd

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,36 @@
 class Customer
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        waiters = []
+        Meal.all.each do |meal|
+            if meal.customer == self
+                waiters << meal.waiter
+            end
+        end
+        waiters
+    end
   
 end
